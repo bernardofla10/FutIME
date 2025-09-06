31b9b1cd3f65669f940427df24d258fff2e91adc
bernardofla10 <decastro@ime.eb.br>
2025-09-06 15:51:41 -0300

    Initial commit: Spring Boot project structure

---
+++ src/main/java/br/com/futime/futime_api/FutImeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.futime.futime_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutImeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutImeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/futime/futime_api/FutImeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futime.futime_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutImeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
