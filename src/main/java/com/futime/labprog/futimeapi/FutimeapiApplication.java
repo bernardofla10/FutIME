a1c416b04fd3c39c5f29aa3c613cf31a92db18d8
bernardofla10 <decastro@ime.eb.br>
2025-10-14 21:48:19 -0300

    chore(backend): inicia configuração do projeto Spring Boot

---
+++ src/main/java/com/futime/labprog/futimeapi/FutimeapiApplication.java
@@ -0,0 +1,13 @@
+package com.futime.labprog.futimeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutimeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutimeapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/futime/labprog/futimeapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.futime.labprog.futimeapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FutimeapiApplication.class);
+	}
+
+}
