fbe1a8339f8c3862a7e5bccfa92ec60d6eecde09
bernardofla10 <decastro@ime.eb.br>
2025-10-14 21:48:19 -0300

    chore(backend): inicia configuração do projeto Spring Boot

---
+++ src/test/java/com/futime/labprog/futimeapi/FutimeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futime.labprog.futimeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutimeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
