887a5e476058702ccd05cda0a1b69dc6e892c320
bernardofla10 <decastro@ime.eb.br>
2025-11-20 12:27:27 -0300

    feat: implementa script para popular o banco de dados

--- src/main/java/com/futime/labprog/futimeapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/futime/labprog/futimeapi/exception/GlobalExceptionHandler.java
@@ -54,6 +54,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    @ExceptionHandler(org.springframework.web.servlet.resource.NoResourceFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(
+            org.springframework.web.servlet.resource.NoResourceFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<StandardError> database(Exception e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
